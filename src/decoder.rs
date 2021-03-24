499940e844639f2d9f5d9b59ce6dfff80f1bc3de
Philipp Mildenberger <philipp.mildenberger@artfabrik.at>
2021-03-24 17:38:10 +0100

    Fix issue with 'get_i16l' (slice only contained 1 byte instead of required 2 bytes)

--- src/decoder.rs
+++ src/decoder.rs
@@ -208,7 +208,7 @@ mod decoder_trait {
                 channels = *extradata.get(9).unwrap_or(&2) as usize;
 
                 if extradata.len() >= OPUS_HEAD_SIZE {
-                    gain_db = get_i16l(&extradata[16..17]);
+                    gain_db = get_i16l(&extradata[16..18]);
                     channel_map = extradata[18] != 0;
                 }
                 if extradata.len() >= OPUS_HEAD_SIZE + 2 + channels {
